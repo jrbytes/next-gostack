48133e1c959f08c64baf96a5dff458c0154fbaee
Jr Bytes <itamazor@gmail.com>
2020-10-22 12:23:02 -0300

    Added dynamic import.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from 'next'
+import math from '../lib/math'
 import { Title } from '../styles/pages/Home'
 
 interface IProduct {
@@ -11,6 +12,12 @@ interface HomeProps {
 }
 
 export default function Home({ recommendedProducts }: HomeProps) {
+  async function handleSum() {
+    const math = (await import('../lib/math')).default
+
+    alert(math.sum(3, 5))
+  }
+
   return (
     <div>
       <section>
@@ -22,6 +29,8 @@ export default function Home({ recommendedProducts }: HomeProps) {
           ))}
         </ul>
       </section>
+
+      <button onClick={handleSum}>Sum!</button>
     </div>
   )
 }
